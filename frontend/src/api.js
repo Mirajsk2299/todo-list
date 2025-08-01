a55e1fbe814aa2758538828a4f34caf913ea2d59
Miraj Shaikh <98844021+Mirajsk2299@users.noreply.github.com>
2025-08-02 01:35:41 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://todo-list-backend-1f5a.onrender.com" });
+
+const API = axios.create({ baseURL: "https://todo-list-backend-1f5a.onrender.com/todos" });
 
 export const getTodos = () => API.get("/");
 export const addTodo = (text) => API.post("/", { text });
 export const updateTodo = (id, updated) => API.put(`/${id}`, updated);
 export const deleteTodo = (id) => API.delete(`/${id}`);
-      

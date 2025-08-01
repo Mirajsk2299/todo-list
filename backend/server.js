2361067230350bf7f1933040e804291d06fde7f3
Miraj Shaikh <98844021+Mirajsk2299@users.noreply.github.com>
2025-08-02 01:51:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const todoRoutes = require("./routes/todoRoutes");
-require("dotenv").config();  
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("MongoDB Atlas Connected"))
   .catch(err => console.log("MongoDB connection error:", err));
 

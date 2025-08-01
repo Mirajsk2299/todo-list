e70a9c5b189288324126a96fd6249eeaf996747c
Miraj Shaikh <98844021+Mirajsk2299@users.noreply.github.com>
2025-08-02 01:31:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
+  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
   .then(() => console.log("MongoDB Atlas Connected"))
   .catch(err => console.log("MongoDB connection error:", err));
 

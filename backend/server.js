07716b6730cc8a3d402fff5e18f08f72d3dfc5e2
Miraj Shaikh <98844021+Mirajsk2299@users.noreply.github.com>
2025-08-20 01:42:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,10 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection (IMPORTANT: use MONGO_URI not MONGO_URL)
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -19,14 +18,11 @@ mongoose
   .then(() => console.log("✅ MongoDB Atlas Connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
-// Routes
 app.use("/todos", todoRoutes);
 
-// Default route to test backend is alive
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
